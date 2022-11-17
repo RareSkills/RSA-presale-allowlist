9f8e48be6b5b0d8e4854237d9a6403f8acc51b1e
MB <michael.dburke56@gmail.com>
2022-11-16 18:36:08 -0600

    Log important vars from RSA script.
    Copy code into memory via assembly. Implement tests to view specific pieces of this code.

--- NOTES.md
+++ NOTES.md
@@ -4,7 +4,7 @@
 
 The over arching goal here is to create a more gas efficient allow listing system. To create a proof of concept and test we will need to:
 - [x] Spin up a simple project that involves minting
-- [ ] Lable `rsa.py` output more clearly (This will help transpose RSA proving function into solidity)
+- [x] Label `rsa.py` output more clearly (This will help transpose RSA proving function into solidity)
 - [ ] Implement functionality to prove signature (See `Using RSA for minting` below)
 - [ ] Implement reading/parsing large values (1024bit) from outside contracts (See `ExampleMulMod.sol` in ./contracts)
 - [ ] Utalize the `metamorphic` pattern for mutating this public key (The large value from above)
@@ -13,7 +13,7 @@ The over arching goal here is to create a more gas efficient allow listing syste
 - [ ] The public key is stored in the bytecode of another smart contract because reading 1024 bits from storage (the RSA public key size) would make the gas cost go up a lot
 - [ ] To invalidate the public key, the contract must be selfdestructed and different bytecode must be deployed to the same address using the metamorphic pattern.
 - [ ] To read the public key, you’ll need to do extcodecopy and ignore the executable bytecode at the beginning. You can also use address.code but I somewhat suspect this kind of an operation is actually easier in assembly than solidity
-- [ ] The address of the external contract should be immutable, or you’ll get another storage read. The contents are not immutable because the contract is metamorphic
+- [x] The address of the external contract should be immutable, or you’ll get another storage read. The contents are not immutable because the contract is metamorphic
 - [ ] Once you get all that working, add access lists to knock off another 100 gas. I can share working code for that, but the idea is here (https://hackmd.io/@fvictorio/gas-costs-after-berlin)
 
 
--- test/rsaPresale.js
+++ test/rsaPresale.js
@@ -1,15 +1,34 @@
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 const { expect } = require("chai");
 
+const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+const KEYSTORY_BYTECODE = '608060405260008060006101000a81548173ffffffffffffffffffffffffffffffffffffffff021916908373ffffffffffffffffffffffffffffffffffffffff160217905550348015605057600080fd5b5060a48061005f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806383197ef014602d575b600080fd5b60336035565b005b60008054906101000a900473ffffffffffffffffffffffffffffffffffffffff1673ffffffffffffffffffffffffffffffffffffffff16fffea26469706673582212200b7435d044ce1581c087230e6ead7bf275070e9cbeb54836d1e94c644f05e63764736f6c63430008110033';
+
+
 describe("RSA presale allowlist", function () {
-  async function deployFixture() {
-    const [owner, user1] = await ethers.getSigners();
+    async function deployFixture() {
+        const [owner, user1] = await ethers.getSigners();
 
-    const TestTokenFactory = await ethers.getContractFactory("TestToken");
-    const testToken = await TestTokenFactory.deploy();
+       /**
+        * At this time the token is being deployed w/ the zero address as a 
+        * contructor arg. This will later be changes to the address of the metamorphic
+        * contract containing the public key value.
+        */
+        const TestTokenFactory = await ethers.getContractFactory("TestToken");
+        const testToken = await TestTokenFactory.deploy(ZERO_ADDRESS);
 
-    return { testToken, owner, user1 };
-  }
+        const KeyStoreFactory = await ethers.getContractFactory("KeyStore");
+        const keyStore = await KeyStoreFactory.deploy();
+        
+        /**
+         * This will need to be renamed. It is currently being used as a 
+         * general utility contract for testing ideas.
+         */
+        const ContractFactory = await ethers.getContractFactory("Contract");
+        const contract = await ContractFactory.deploy();
+
+        return { testToken, keyStore, contract, owner, user1 };
+    }
 
     describe("Deployment", function () {
         it("TestToken returns name as expected", async function () {
@@ -18,5 +37,41 @@ describe("RSA presale allowlist", function () {
 
             expect(tokenName).to.equal("TestToken");
         });
+    });
+
+    describe("Reading from external contract", function () {
+        it("The 'read' function should return data from the passed in contract", async function () {
+            const { testToken, keyStore, contract, owner } = await loadFixture(deployFixture);
+
+            const code = await contract._readFromAddress(keyStore.address, 1);
+            console.log(`code: ${code}`);
+            expect(code).to.not.equal("");
+        });
+
+        it("Returns address", async function () {
+            const { testToken, keyStore, contract, owner } = await loadFixture(deployFixture);
+          
+            const newAddress = await contract.callStatic.deployAsBytecode('99999999999');
+            console.log(`newAddress: ${newAddress}`);
+
+            const code = await contract._readFromAddress(newAddress, 1);
+            console.log(`code: ${code}`);
+
+            expect(true).to.equal(true);
+        });
+
+        it("Returns code from specific memory slot", async function () {
+            const { testToken, keyStore, contract, owner } = await loadFixture(deployFixture);
+          
+            const memSlot8 = await contract.callStatic.deployAndReadSlot('99999999999', 8);
+            console.log(`memSlot8 : ${memSlot8}`); 
+
+            const memSlot9 = await contract.callStatic.deployAndReadSlot('99999999999', 9);
+            console.log(`memSlot9 : ${memSlot9}`); 
+
+            const memSlot10 = await contract.callStatic.deployAndReadSlot('99999999999', 10);
+            console.log(`memSlot10: ${memSlot10}`); 
+
+            expect(true).to.equal(true); });
     });
 });

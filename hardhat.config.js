9cd2182f799252a1baf77b610ae8979bef5b4935
Suthan Somadeva <106453938+PeakyCryptos@users.noreply.github.com>
2022-11-26 22:06:12 -0500

    codecopy test

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,17 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-gas-reporter");
+require('dotenv').config();
+
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
+  }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -5,12 +5,30 @@
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
 const hre = require("hardhat");
+/* byte code paths */
+const abi = require("../abis/RsaCodeCopyDemoABI.json"); 
+const bytecode = require("../contracts/RsaCodeCopyDemo.json");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
 
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const Contract = await hre.ethers.getContractFactory(abi, bytecode);
+  //const Contract = await ethers.getContractFactory("RsaCopyCodeDemo");
+  const contract = await Contract.deploy(
+    "0x2323232323232323232323232323232323232323232323232323232323232323",//salt
+    "0x0000000000000000000000000000000000000000000000000000000000000003"//exponent
+  );
+
+  console.log("contract address:", contract.address);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });

cfaf5b1cd4116ab0d9f134e045ce02274025b41b
MB <michael.dburke56@gmail.com>
2022-12-03 17:59:53 -0600

    Format comments to natspec.

--- contracts/Rsa.sol
+++ contracts/Rsa.sol
@@ -4,9 +4,6 @@ pragma solidity 0.8.17;
 contract Rsa {
     event Metamorphosed(address metamorphicContract);
 
-    bytes32 public constant EXPONENT =
-        0x0000000000000000000000000000000000000000000000000000000000000003;
-
     address public immutable owner;
 
     address public immutable metamorphicContractAddress;
@@ -17,26 +14,32 @@ contract Rsa {
 
     bytes currentImplementationCode;
 
+    bytes32 private constant EXPONENT =
+        0x0000000000000000000000000000000000000000000000000000000000000003;
+
     /**
-     * @dev See README.md for bytecode breakdown
+     * @dev See README.md for bytecode breakdown.
      */
     bytes private constant _metamorphicContractInitializationCode = (
         hex"630000000e60005261017760006004601c335afa6101376040f3"
     );
 
     /**
-     * @dev METAMORPHIC_INIT_HASH is a calculated value based on '_metamorphicContractInitializationCode'
-     *      If the code is updated, be sure to recalculate the hash.
+     * @dev METAMORPHIC_INIT_HASH is a calculated value based on 
+     *      '_metamorphicContractInitializationCode'.
+     *
+     *      If the metamorphic code is updated, be sure to recalculate the hash.
      *
      *      keccak256(abi.encodePacked(_metamorphicContractInitializationCode)
      */
     bytes32 private constant METAMORPHIC_INIT_HASH =
         0xc24922855851a254a6fc4fc08e7ae8481ab58d05b2e4f607575d845e559d69ba;
 
-    //------------------------------------------------------------\\
+//----------------------------------------------------------------------------\\
     constructor(bytes32 _salt, uint256 _modLength) {
         owner = msg.sender;
         salt = _salt;
+        modLength = _modLength;
 
         metamorphicContractAddress = (
             address(
@@ -54,19 +57,22 @@ contract Rsa {
                 )
             )
         );
-
-        modLength = _modLength;
     }
 
     /**
-     * @notice 'verifySignature' is the user facing function used to validate signed messages
+     * @notice 'verifySignature' is the user facing function used to validate 
+     *         signed messages.
      *
-     * @param 'sig' length must always be equal to the length of the public key(modulus)
+     * @param 'sig' is the signed message. It's length must always be equal to 
+     *         the length of the public key(modulus).
      *
-     * @dev Exponent is hardcoded at top of contract. It should always be 32 bytes
+     * @dev    Exponent is hardcoded at top of contract. It should always be 32 
+     *         bytes in length.
      *
-     * @dev See below layout & link for memory when calling precompiled modular exponentiation contract (0x05)
-     *      <length_of_BASE(signature)> <length_of_EXPONENT> <length_of_MODULUS> <BASE(signature)> <EXPONENT> <MODULUS>
+     * @dev    See below layout & link for memory when calling precompiled 
+     *         modular exponentiation contract (0x05).
+     *
+     * <length_of_BASE(signature)> <length_of_EXPONENT> <length_of_MODULUS> <BASE(signature)> <EXPONENT> <MODULUS>
      *
      *      https://github.com/ethereum/EIPs/blob/master/EIPS/eip-198.md
      */
@@ -77,51 +83,67 @@ contract Rsa {
         address _metamorphicContractAddress = metamorphicContractAddress;
 
         assembly {
-            // no need to update pointer as all memory written here can be overwriten with no consequence 
-            // Store in memory, length of BASE(signature), EXPONENT, MODULUS
+            /** 
+             * @dev No need to update free memory pointer as all memory written here 
+             *      can be overwriten with no consequence.
+             *
+             * @dev Store in memory, length of BASE(signature), EXPONENT, MODULUS.
+             */
             mstore(0x80, sig.length)
             mstore(add(0x80, 0x20), 0x20)
             mstore(add(0x80, 0x40), sig.length)
 
-            // BASE: The signature
-            calldatacopy(0xe0, sig.offset, sig.length)
-
-            // EXPONENT hardcoded to 3
-            mstore(add(0xe0, sig.length), EXPONENT)
-
-            // Calculate where in memory to copy modulus to
-            // 0x37 -> offset of where the signature begins in the metamorphic bytecode
+            /**
+             * @dev Calculate where in memory to copy modulus to (modPos). This must 
+             *      be dynamically determined as various size of signature may be used.
+             */
             let modPos := add(0xe0, add(sig.length, 0x20))
 
+            // Store in memory, BASE(signature), EXPONENT, MODULUS(public key).
+            calldatacopy(0xe0, sig.offset, sig.length)
+            mstore(add(0xe0, sig.length), EXPONENT)
+
+            /**
+             * @dev 0x37 is a precalulated value that is the offset of where the 
+             *      signature begins in the metamorphic bytecode.
+             */
             extcodecopy(_metamorphicContractAddress, modPos, 0x37, sig.length)
 
             /**
-             * @dev Call 0x05 precompile (modular exponentation)
-             *
-             * Args:
-             *   gas,
-             *   precomipled contract address,
-             *   memory pointer of begin of calldata,
-             *   size of call data,
-             *   pointer for where to copy return,
-             *   size of return data
+             * @dev callDataSize must be dynamically calculated. It follows the 
+             *      previously mentioned memory layout including the length and
+             *      value of the sig, exponent and modulus.
              */
-
-            // lengths of Signature + Base + Exponent = 0x60...
-            // Added with space of storage occupied by exponent, modulus, and signature...
-            // Which is 0x20 + (sig.length * 2) as modulus length will always == signature length  
             let callDataSize := add(0x80, mul(sig.length, 2))
 
+            /**
+             * @dev Call 0x05 precompile (modular exponentation) w/ the following
+             *      args and revert on failure.
+             *
+             *      Args:
+             *      gas,
+             *      precomipled contract address,
+             *      memory pointer of begin of calldata,
+             *      size of call data (callDataSize),
+             *      pointer for where to copy return,
+             *      size of return data
+             */
             if iszero(staticcall(gas(), 0x05, 0x80, callDataSize, 0x80, sig.length)) {
                 revert(0, 0)
             }
 
-            // decoded signature is always stored in the last 32 bytes of return data
-            // sig.length + 0x80 is the end of the return data
-            // 0x60 + sig.length will give you the last 32 bytes 
+            /**
+             * @dev Parse return value from modular exponentation calculation.
+             *      This calculation will load the correct 32bytes of memory 
+             *      onto the stack.
+             */
             let decodedSig := mload(add(0x60, sig.length))
 
-            // Check bit mask of return data. Ensure it is a valid address (first 12 bytes are zero)
+            /**
+             * @dev Use bit mask of decodedSig to ensure it is a valid address.
+             *      If the user has passed a valid sig, this will be a 20 byte
+             *      address, left padded to 32 bytes, and revert if not valid.
+             */
             if iszero(
                 iszero(
                     and(
